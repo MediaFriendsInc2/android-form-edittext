6d7cdecbda7e211b113986013a775c4b4daf5c1c
Nick Lizotte <lizottenj@gmail.com>
2013-09-11 14:16:39 -0400

    Allow numbers and Apostrophe's in names

--- library/src/com/andreabaccega/formedittextvalidator/PersonNameValidator.java
+++ library/src/com/andreabaccega/formedittextvalidator/PersonNameValidator.java
@@ -3,6 +3,6 @@ package com.andreabaccega.formedittextvalidator;
 public class PersonNameValidator extends RegexpValidator {
 	public PersonNameValidator(String message) {
 		// will allow people with hyphens in his name or surname. Supports also unicode
-		super(message, "[\\p{L}-]+");
+		super(message, "[\\p{L}\\p{N}-\']+");
 	}
 }
