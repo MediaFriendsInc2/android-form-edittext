88b5c2b9dbfff4e3f5f92552d72641cdfcc588e1
Nick Lizotte <lizottenj@gmail.com>
2014-02-24 16:51:48 -0500

    HEYW-111 Add underscore as an allowed character in names

--- library/src/com/andreabaccega/formedittextvalidator/PersonFullNameValidator.java
+++ library/src/com/andreabaccega/formedittextvalidator/PersonFullNameValidator.java
@@ -3,6 +3,6 @@ package com.andreabaccega.formedittextvalidator;
 public class PersonFullNameValidator extends RegexpValidator {
 	public PersonFullNameValidator(String message) {
 		// will allow people with hyphens in his name or surname. Supports also unicode
-		super(message, "[\\p{L}-\' ]+");
+		super(message, "[\\p{L}-\'_ ]+");
 	}
 }
